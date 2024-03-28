5652f7e86be0637a7d0efb44536edfe0a766c133
Prajotp <prajot.pawar@portalwiz.com>
2024-03-28 11:26:36 +0530

    Changes in yml file

--- mydrfproject/deployment.py
+++ mydrfproject/deployment.py
@@ -1,42 +1,41 @@
 import os
-from .settings import BASE_DIR
-
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSR_TRUSTED_ORIGINS = ["https://" + os.environ['WEBSITE_HOSTNAME']]
-DEBUG = False
-SECRET_KEY = os.environ["MY_SECRET_KEY"]
+from.settings import *
+from.settings import BASE_DIR
+ALLOWED_HOSTS=[os.environ['WEBSITE_HOSTNAME']]
+CSR_TRUSTED_ORIGINS =["https://"+os.environ['WEBSITE_HOSTNAME']]
+DEBUG=False
+SECRET_KEY = os.environ["MY_SECRETE_key"]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+# CORS_ALLOWED_ORIGIND=[
 
+# ]
 STORAGES = {
+
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
-
-CONNECTION = os.environ['AZURE_MYSQL_CONNECTION_STRING']
-CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1] for pair in CONNECTION.split(';')}
+CONNECTION=os.environ('azuar_postagral_sql')
+CONNECTION_STR={pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": CONNECTION_STR['Database'],
-        "HOST": CONNECTION_STR['Host'],
-        "USER": CONNECTION_STR['User'],
-        "PASSWORD": CONNECTION_STR['Password'],
-        "PORT": CONNECTION_STR.get('Port', '3306'),  # Default MySQL port
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": CONNECTION_STR['dbname'],
+         "HOST": CONNECTION_STR['host'],
+        "USER":CONNECTION_STR['user'],
+        "PASSWORD": CONNECTION_STR['password'],
+       
+        "PORT": "5432",
     }
 }
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT=BASE_DIR/'staticfiles'
